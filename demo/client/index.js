c921995b4063ed1940732cffacc7fe3724917869
Atsushi Nagase <a@ngs.io>
2016-12-06 14:54:34 +0900

    Fork ribbon

--- demo/client/index.js
+++ demo/client/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import DemoEditor from './components/DemoEditor';
+import Ribbon from 'react-github-fork-ribbon';
 
 // Import your routes so that you can pass them to the <Router /> component
 // eslint-disable-next-line import/no-named-as-default
@@ -8,7 +9,12 @@ import DemoEditor from './components/DemoEditor';
 // Only render in the browser
 if (typeof document !== 'undefined') {
   render(
-    <DemoEditor />,
+    <div>
+      <Ribbon href="https://github.com/ngs/draft-js-markdown-shortcuts-plugin/" target="_blank" position="right" color="black">
+        Fork me on GitHub
+      </Ribbon>
+      <DemoEditor />
+    </div>,
     document.getElementById('root')
   );
 }
