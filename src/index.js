bb373470f5330585740d97a0b97a59b7835f1cda
Grant Nestor <grantnestor@gmail.com>
2018-02-09 03:26:03 -0800

    Don't handleInlineStyle in code-block

--- src/index.js
+++ src/index.js
@@ -21,13 +21,18 @@ import { replaceText } from './utils';
 
 function checkCharacterForState(editorState, character) {
   let newEditorState = handleBlockType(editorState, character);
+  const contentState = editorState.getCurrentContent();
+  const selection = editorState.getSelection();
+  const key = selection.getStartKey();
+  const currentBlock = contentState.getBlockForKey(key);
+  const type = currentBlock.getType();
   if (editorState === newEditorState) {
     newEditorState = handleImage(editorState, character);
   }
   if (editorState === newEditorState) {
     newEditorState = handleLink(editorState, character);
   }
-  if (editorState === newEditorState) {
+  if (editorState === newEditorState && type !== 'code-block') {
     newEditorState = handleInlineStyle(editorState, character);
   }
   return newEditorState;
