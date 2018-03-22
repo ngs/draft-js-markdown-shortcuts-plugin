b7a8908e19dd8643e4f0a557d56cfbeaba64cc3f
Max Stoiber <contact@mxstbr.com>
2018-03-22 16:51:15 +0100

    Remove unused comment

--- src/modifiers/__test__/handleInlineStyle-test.js
+++ src/modifiers/__test__/handleInlineStyle-test.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 
-//import { expect } from "chai";
 import Draft, { EditorState, SelectionState } from "draft-js";
 import handleInlineStyle from "../handleInlineStyle";
 
